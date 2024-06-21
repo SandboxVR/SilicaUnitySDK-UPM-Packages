eefa3933031d38f9a72b5c8c52b957a3929f0888
Cyril <cyril.chu@sandboxvr.com>
2024-06-21 12:07:24 +0800

    feat: update files

--- Packages/sandboxvr.silica.props/Runtime/Props.cs
+++ Packages/sandboxvr.silica.props/Runtime/Props.cs
@@ -8,12 +8,11 @@ public class Props : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Props Update");
+        UnityEngine.Debug.Log("Props Update");
     }
 }
